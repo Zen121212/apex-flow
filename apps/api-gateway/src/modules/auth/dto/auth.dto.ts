ca487b9c9798ed2a17174c3d84cd4ebdc0e941e6
Zein Kassem <zein.s.kassem@gmail.com>
2025-09-05 22:55:59 +0300

    Add authentication module with JWT support

---
+++ apps/api-gateway/src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,33 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class AuthResponseDto {
+  user: {
+    id: string;
+    email: string;
+    name: string;
+    provider: string;
+  };
+  token: string;
+}
