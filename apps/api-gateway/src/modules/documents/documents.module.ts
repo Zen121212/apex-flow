fdc08b4b6d8bf6c07490fa9ade99e79d8d0c2c9c
Zein Kassem <zein.s.kassem@gmail.com>
2025-09-05 22:53:38 +0300

    Update documents module with improved service configuration

--- apps/api-gateway/src/modules/documents/documents.module.ts
+++ apps/api-gateway/src/modules/documents/documents.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { DocumentMinimal as Document } from '../../entities/document-minimal.entity';
+import { WorkflowsModule } from '../workflows/workflows.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Document]),
+    WorkflowsModule
+  ],
   controllers: [DocumentsController],
   providers: [DocumentsService],
+  exports: [DocumentsService]
 })
 export class DocumentsModule {}
