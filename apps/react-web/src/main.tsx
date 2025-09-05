a1ffd09915b54557da5e3a085477b81423cad5b6
Zein Kassem <zein.s.kassem@gmail.com>
2025-09-05 22:54:47 +0300

    Update React main.tsx entry point

--- apps/react-web/src/main.tsx
+++ apps/react-web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './styles/globals.css';
+import './components/atoms/Button/Button.css'; // Preload button styles
 import App from './App';
 
 const root = createRoot(document.getElementById('root')!);
